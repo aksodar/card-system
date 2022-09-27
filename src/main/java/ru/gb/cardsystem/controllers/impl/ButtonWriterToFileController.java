7813548dd88434e304a6d019334c48f70a2a8510
«Mikhail» <kopfgolova@gmail.com>
2022-09-27 16:23:00 +0300

    add Button Txt Write File

---
+++ src/main/java/ru/gb/cardsystem/controllers/impl/ButtonWriterToFileController.java
@@ -0,0 +1,40 @@
+package ru.gb.cardsystem.controllers.impl;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.*;
+
+import ru.gb.cardsystem.controllers.JComponentController;
+import ru.gb.cardsystem.controllers.JComponentWriterController;
+import ru.gb.cardsystem.service.impl.FileWriterService;
+
+public class ButtonWriterToFileController extends JComponentWriterController {
+    
+    private final JComponentController<JTextField> textFieldController;
+    
+    protected ButtonWriterToFileController() {
+        super(new ArrayList<>(), new FileWriterService());
+        this.textFieldController = new TextFieldController();
+        init();
+    }
+
+    private void init() {
+        final JButton writeToFile = new JButton("Write to File");
+        writeToFile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                final Map<String, String> data = new HashMap<>();
+                for (final JTextField textField : textFieldController.getComponents()) {
+                    data.put(textField.getName(), textField.getText());
+                }
+                getWritableService().write(data);
+            }
+        });
+
+        getComponents().add(writeToFile);
+    }
+}
