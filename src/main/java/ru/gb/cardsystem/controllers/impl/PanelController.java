1db7a1680b43b5d04412d7f6d01e5cbc8f54b4c3
RomanGorkavenko <roman@gorkavenko.ru>
2022-09-29 20:30:21 +0300

    WorkSeminar8

--- src/main/java/ru/gb/cardsystem/controllers/impl/PanelController.java
+++ src/main/java/ru/gb/cardsystem/controllers/impl/PanelController.java
@@ -21,12 +21,14 @@ public class PanelController extends JComponentController<JComponent> {
     private final ApplicationContext appContext;
     private final JComponentController<JTextField> textFieldController;
     private final JComponentWriterController<JButton> buttonWriterToLoggerController;
+    private final JComponentWriterController<JButton> buttonWriterToFileController;
 
     public PanelController(final ApplicationContext appContext) {
         super(new ArrayList<>());
         this.appContext = appContext;
         this.textFieldController = new TextFieldController();
         this.buttonWriterToLoggerController = new ButtonWriterToLoggerController();
+        this.buttonWriterToFileController = new ButtonWriterToFileController();
         initTextInputPanel();
         initButtonPanel();
     }
@@ -62,6 +64,10 @@ public class PanelController extends JComponentController<JComponent> {
         buttonPanel.setBorder(BorderFactory.createLineBorder(Color.GRAY));
 
         for (final JButton button : buttonWriterToLoggerController.getComponents()) {
+            buttonPanel.add(button);
+        }
+
+        for (final JButton button : buttonWriterToFileController.getComponents()) {
             buttonPanel.add(button);
         }
 
