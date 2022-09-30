6ab5875f187fc72e35e2182f975d9fad344fce0c
StukalovSV <energostuk@gmail.com>
2022-09-30 14:52:18 +0300

    Add Writer .txt

---
+++ src/main/java/ru/gb/cardsystem/controllers/impl/ButtonWriterToFileController.java
@@ -0,0 +1,42 @@
+package ru.gb.cardsystem.controllers.impl;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
+import ru.gb.cardsystem.controllers.JComponentController;
+import ru.gb.cardsystem.controllers.JComponentWriterController;
+import ru.gb.cardsystem.service.impl.FileWriterService;
+
+public class ButtonWriterToFileController extends JComponentWriterController{
+
+    private final JComponentController<JTextField> textFieldController;
+    
+    protected ButtonWriterToFileController() {
+        super(new ArrayList<>(), new FileWriterService());
+        this.textFieldController = new TextFieldController();
+        init();
+    }
+
+    private void init() {
+        final JButton writeInLogger = new JButton("Write in File");
+        writeInLogger.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                final Map<String, String> data = new HashMap<>();
+                for (final JTextField textField : textFieldController.getComponents()) {
+                    data.put(textField.getName(), textField.getText());
+                }
+                getWritableService().write(data);
+            }
+        });
+
+        getComponents().add(writeInLogger);
+    }
+    
+}
